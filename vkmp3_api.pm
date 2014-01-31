ddb81cfca0c569a0a507e794a9b124b92d1371d4
builat <rf00rm@gmail.com>
2014-01-31 17:26:50 +0700

    Update vkmp3_api.pm
    With changed VK markup hear we go with changed vk music download.

--- vkmp3_api.pm
+++ vkmp3_api.pm
@@ -2,7 +2,7 @@
 package mp3vk_api;
 use strict;
 use warnings;
- 
+
 use Env qw(HOME);
 use utf8;
 use Encode;
@@ -12,14 +12,14 @@ use LWP;
 use LWP::Protocol::https;
 use LWP::Simple;
 use Parallel::ForkManager;
- 
-our $VERSION = 0.1;
- 
+
+our $VERSION = 0.2;
+
 #constructor
 sub new {
   my ($class, %args) = @_;
      return ('Error in syntax usage') if(_arg_validation(\%args) != 1);
- 
+
 my $self = { 
       ua => _mk_ua(),
       login => $args{login},
@@ -29,12 +29,12 @@ my $self = {
 };
     
   bless $self, $class;
- 
-  return () if($self->_connection_try() != 1);
- 
+
+  die ('ERROR WITH CONNECTION AUTH') if($self->_connection_try() != 1);
+
   return $self;
 }
- 
+
 #Creation of user agent. Private method
 sub _mk_ua {
 	my $ua = LWP::UserAgent->new();
@@ -42,38 +42,39 @@ sub _mk_ua {
 	$ua->cookie_jar( {} );
 return ($ua);
 }
- 
+
 #public method. search via vk.
 sub search {
   my ($self, $query) = @_;
   my $res = $self->{ua}->get('http://vk.com/search?c[section]=audio&c[q]='.uri_escape_utf8($query));
   eval{$res->is_success;} or return 'Error possible with LWP_ua see err msg -> '.$res->status_line;
-          my @findnings = $res->decoded_content =~  m'<input type="hidden" id="audio_info(.*?)</table>'sgi;
+#          my @findnings = $res->decoded_content =~  m'<input type="hidden" id="audio_info(.*?)</table>'sgi;
+          my @findnings = $res->decoded_content =~  m'<div id="audio.*?>(.*?)<div class="ai_controls">'sgi;
+
                         my @dump;
                         push @dump, $self->_cleanup_response($_) for(@findnings);
                               @dump = grep { defined $_ } @dump;
   return (\@dump);
 }
- 
+
 #parsing of vk response. private method
 sub _cleanup_response {
   my ($self, $str) = @_;
-  my ($title) = $str =~ m{<div class="title_wrap fl_l".*?>(.*?)</div>}si;
+
+  my ($title) = $str =~ m{<span class="ai_title">(.*?)</span>}si;
   return undef unless $title;
- 
   $title =~ s/(<.*?>|\(.+\)|\&\w+|[;'"])//g;
   $title =~ s/\s+/ /g;
   $title = decode_entities($title);
- 
-  my ($duration) = $str =~ m{<div class="duration fl_r".*?>(\d+:\d+)</div>}i;
+  my ($duration) = $str =~ m{<div class="ai_dur" data-dur="\d+" onclick="audioplayer.switchTimeFormat(this, event);">(\d+:\d+)</div>}i;
+
   my ($link) = $str =~ m{value="(http://[^'"`]+\.mp3)}i;
- 
   $duration = 0 unless($duration);
   
   return { title => $title, duration => $duration, link => $link };
 }
- 
- 
+
+
 #check posibility of connection
 sub _connection_try{
   my $self = shift;
@@ -81,16 +82,16 @@ sub _connection_try{
       email => $self->{login},
       pass => $self->{password},
     });  
- 
+
   if(  $res->is_success &&
       ($res->decoded_content =~ /var\s+vk\s*=\s*\{[^\{\}]*?id\s*\:\s*(\d+)/i
-       || $res->decoded_content =~ m{login\.vk\.com/\?act=logout}i ) ) {
+       || $res->decoded_content =~ m#login\.vk\.com/\?act=logout#i ) ) {
     $self->{id} = $1;
     return 1;
   }
   return 0;
 }
- 
+
 #validation of login and passwd
 sub _arg_validation {
         my $args = shift;
@@ -110,26 +111,24 @@ sub _arg_validation {
                 return 1;
         }
 }
- 
- 
+
+
 sub vk_download{
     my $self = shift;
     my $ref_dump = shift;
     my $pm = Parallel::ForkManager->new($self->{threads});
-    my $inform;
-      foreach (@{$ref_dump}) {
-                                                  $pm->start and next; # do the fork
-                                                   if (getstore($_->{link}, "$self->{dir}/$_->{title}.mp3") != RC_OK){
-                                                                  print 'prepareing to complite query ->'.$self->{dir}."\n";
+          foreach (@{$ref_dump}) {
+         
+                                                   $pm->start and next; # do the fork
+                                                    if (getstore($_->{link}, "$self->{dir}/$_->{title}.mp3") != RC_OK){
+                                                                  print 'error occured '."\n";
                                                     }
-                                                    else{
-                                                      $_->{title} = encode('utf8', $_->{title});
-                                                      print $_->{title} . '.................. downloaded'."\n";
-                                                    }                
+                                                    else{  
+                                                                 print  $_->{title}.'.................. downloaded'."\n";
+                                                    }      
                                                   $pm->finish; # do the exit in the child process
-                                                   
   }
- 
+
        
 }
 1;
